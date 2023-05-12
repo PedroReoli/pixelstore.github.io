01c4cb4c59137ea2544899253ed7c996e56bb687
PedroReoli <128660097+PedroReoli@users.noreply.github.com>
2023-05-12 11:33:14 -0300

    att 12/05

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,5 @@
 
-let count = 1;
-document.getElementById("radio1").checked = true;
 
-setInterval( function(){
-  nextImage();
-}, 5000)
-
-function nextImage(){
-  count++;
-  if (count>3){
-    count = 1;
-  }
-
-  document.getElementById("radio"+count).checked = true;
-
-}
 // Cria um array vazio para armazenar os produtos
 const produtos = [];
 
